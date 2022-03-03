bb30a20f9d2da5695f1dece12bc04e45e81cd58a
Sonu0911 <galchelwarrushikesh1111@gmail.com>
2022-03-03 08:43:37 +0530

    referencePopulate

---
+++ src/models/bookModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const bookSchema = new mongoose.Schema({
+    name: String,
+    author: { type: ObjectId, ref: "NewAuthor" },
+    price: Number,
+    ratings: Number,
+    publisher: { type: ObjectId, ref: "NewPublisher" }
+}, { timestamps: true });
+
+module.exports = mongoose.model('NewBook', bookSchema)
---
+++ src/routes/route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const authorController = require("../controllers/authorController")
+const bookController = require("../controllers/bookController")
+const publisherController = require("../controllers/publisherController")
+
+router.post("/createAuthor", authorController.createAuthor)
+router.post("/createPublisher", publisherController.createPublisher)
+router.post("/createBook", bookController.createBook)
+router.get("/getBooks", bookController.getBooks)
+
+module.exports = router;
