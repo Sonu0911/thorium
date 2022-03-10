2fe683754093d48cb9d54f37f4c725f8c1a27eee
Sonu0911 <galchelwarrushikesh1111@gmail.com>
2022-03-10 08:28:55 +0530

    authorisationAssignment

---
+++ src/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+const userModel = require("../models/userModel")
+
+const authenticate = function(req, res, next) {
+    //check the token in request header
+    //validate this token
+
+    let token = req.headers["x-auth-token"]
+    if (!token) return res.send({ status: false, msg: "token must be present in the request header" })
+    let decodedToken = jwt.verify(token, 'functionup-thorium')
+
+    if (!decodedToken) return res.send({ status: false, msg: "token is not valid" })
+
+    next()
+};
+
+
+const authorise = function(req, res, next) {
+    // comapre the logged in user's id and the id in request
+    let userToBeModified = req.params.userId
+    let token = req.headers["x-auth-token"]
+    let decodedToken = jwt.verify(token, 'functionup-thorium')
+    let userLoggedIn = decodedToken.userId
+    if (userToBeModified != userLoggedIn) return res.send({ status: false, msg: "user logged is not allowed " })
+    next()
+}
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
